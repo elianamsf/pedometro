1ffb6132ecd7fa1b4e1dce5283296f4dd7fc9c13
Eliana Maria <elianamsfcontatos@gmail.com>
2019-09-25 10:01:54 -0300

    Desafio 3 concluído

--- pedometro/ih-codes/src/main.cpp
+++ pedometro/ih-codes/src/main.cpp
@@ -17,21 +17,18 @@
 //      2012-06-04 - remove accel offset clearing for better results (thanks Sungon Lee)
 //      2012-06-01 - fixed gyro sensitivity to be 2000 deg/sec instead of 250
 //      2012-05-30 - basic DMP initialization working
-
+ 
 /* ============================================
 I2Cdev device library code is placed under the MIT license
 Copyright (c) 2012 Jeff Rowberg
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -41,90 +38,100 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ===============================================
 */
-
+#include <Arduino.h>
+#include <jled.h>
+ 
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+ 
+ 
+char auth[] = "2sfTyt-Bn-xz78ee1VkUnGL3sSd_rYlT";
+char ssid[] = "jualabs"; //nome da rede
+char pass[] = "jualabsufrpe"; //senha da rede
+ 
 #include <Arduino.h>
 // I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
 // for both classes must be in the include path of your project
 #include "I2Cdev.h"
-
+ 
 #include "MPU6050_6Axis_MotionApps20.h"
 //#include "MPU6050.h" // not necessary if using MotionApps include file
 #include "MPU6050.h"
-
+#define BLYNK_PRINT Blynk
+ 
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
 // is used in I2Cdev.h
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
 #endif
-
+ 
 // class default I2C address is 0x68
 // specific I2C addresses may be passed as a parameter here
 // AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
 // AD0 high = 0x69
 MPU6050 mpu;
 //MPU6050 mpu(0x69); // <-- use for AD0 high
-
+ 
 /* =========================================================================
    NOTE: In addition to connection 3.3v, GND, SDA, and SCL, this sketch
    depends on the MPU-6050's INT pin being connected to the Arduino's
    external interrupt #0 pin. On the Arduino Uno and Mega 2560, this is
    digital I/O pin 2.
  * ========================================================================= */
-
+ 
 /* =========================================================================
    NOTE: Arduino v1.0.1 with the Leonardo board generates a compile error
    when using Serial.write(buf, len). The Teapot output uses this method.
    The solution requires a modification to the Arduino USBAPI.h file, which
    is fortunately simple, but annoying. This will be fixed in the next IDE
    release. For more info, see these links:
-
    http://arduino.cc/forum/index.php/topic,109987.0.html
    http://code.google.com/p/arduino/issues/detail?id=958
  * ========================================================================= */
-
-
-
+ 
+ 
+ 
 // uncomment "OUTPUT_READABLE_QUATERNION" if you want to see the actual
 // quaternion components in a [w, x, y, z] format (not best for parsing
 // on a remote host such as Processing or something though)
 //#define OUTPUT_READABLE_QUATERNION
-
+ 
 // uncomment "OUTPUT_READABLE_EULER" if you want to see Euler angles
 // (in degrees) calculated from the quaternions coming from the FIFO.
 // Note that Euler angles suffer from gimbal lock (for more info, see
 // http://en.wikipedia.org/wiki/Gimbal_lock)
 //#define OUTPUT_READABLE_EULER
-
+ 
 // uncomment "OUTPUT_READABLE_YAWPITCHROLL" if you want to see the yaw/
 // pitch/roll angles (in degrees) calculated from the quaternions coming
 // from the FIFO. Note this also requires gravity vector calculations.
 // Also note that yaw/pitch/roll angles suffer from gimbal lock (for
 // more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
 #define OUTPUT_READABLE_YAWPITCHROLL
-
+ 
 // uncomment "OUTPUT_READABLE_REALACCEL" if you want to see acceleration
 // components with gravity removed. This acceleration reference frame is
 // not compensated for orientation, so +X is always +X according to the
 // sensor, just without the effects of gravity. If you want acceleration
 // compensated for orientation, us OUTPUT_READABLE_WORLDACCEL instead.
 //#define OUTPUT_READABLE_REALACCEL
-
+ 
 // uncomment "OUTPUT_READABLE_WORLDACCEL" if you want to see acceleration
 // components with gravity removed and adjusted for the world frame of
 // reference (yaw is relative to initial orientation, since no magnetometer
 // is present in this case). Could be quite handy in some cases.
 //#define OUTPUT_READABLE_WORLDACCEL
-
+ 
 // uncomment "OUTPUT_TEAPOT" if you want output that matches the
 // format used for the InvenSense teapot demo
 // #define OUTPUT_TEAPOT
-
-
-
+ 
+ 
+ 
 #define INTERRUPT_PIN D7  // use pin 2 on Arduino Uno & most boards
 #define LED_PIN D4 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
 bool blinkState = false;
-
+ 
 // MPU control/status vars
 bool dmpReady = false;  // set true if DMP init was successful
 uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
@@ -132,7 +139,7 @@ uint8_t devStatus;      // return status after each device operation (0 = succes
 uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
 uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
-
+ 
 // orientation/motion vars
 Quaternion q;           // [w, x, y, z]         quaternion container
 VectorInt16 aa;         // [x, y, z]            accel sensor measurements
@@ -141,31 +148,37 @@ VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measure
 VectorFloat gravity;    // [x, y, z]            gravity vector
 float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-
+ 
 // packet structure for InvenSense teapot demo
 uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
 // Inicio - Eliana
+WidgetLCD lcd(V0);
 float pitch1;
 float pitch2;
 int qtdPassos;
 boolean mulher = false;
 boolean homem = false;
 float altura;
+float peso;
 float cp; // comprimento do passo
 float dp; // Distância percorrida
-const unsigned long segundo = 1000;
-unsigned long tempo;
-unsigned long veloc; // velocidade
+float veloc; // velocidade
+float metPasso;
+float calorias;
+const float segundos = 5;
+const float horas = 0.0014;
+int qtdPassosInicial = 0 ;
+String saida;
 // Fim - Eliana
-
-
-
+ 
+ 
+ 
 // ================================================================
 // ===               INTERRUPT DETECTION ROUTINE                ===
 // ================================================================
-
+ 
 volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-
+ 
 void ICACHE_RAM_ATTR dmpDataReady() {
     mpuInterrupt = true;
 }
@@ -174,8 +187,6 @@ void ICACHE_RAM_ATTR dmpDataReady() {
 // ===                      Pedometro                          ===
 // ===============================================================
 void countStep(){
-    //Serial.println(pitch1);
-    //Serial.println(pitch2);
     if (abs (pitch2 - pitch1) > 160)
     {
         qtdPassos++;
@@ -187,29 +198,87 @@ void countStep(){
         pitch1 = pitch2;
     }
 }
-float distanciaPecorrida(){
+void distanciaPecorrida(){
     if (mulher){
         cp = altura * 0.415;
     }else if(homem){
         cp = altura * 0.413;
     }
     if (cp >0){
-        dp = cp * qtdPassos; 
+        dp = cp * (qtdPassos - qtdPassosInicial); 
     }
-    return dp;
+    qtdPassosInicial = qtdPassos;
+ 
 }
-float velocidade(){
-    tempo = millis();
-    tempo = tempo/segundo;
-    veloc = tempo;
-    return veloc;
+void velocidade(){
+    veloc = dp/segundos;
 }
+void met(){
+    if (veloc == 0){ // parado
+        metPasso = 1.3;
+    }else if(veloc > 0 && veloc < 6){ // andando
+        metPasso = 3.8;
+    }else if(veloc >= 6){ // Correndo
+        metPasso = 7.5;
+    }
+    calorias += metPasso * peso * horas; 
+}
+void montaSaida(){
+    distanciaPecorrida(); 
+    velocidade();
+    met();
+    lcd.clear();
+    lcd.print(0,0,"DP:");
+    lcd.print(3,0,dp);
+    lcd.print(8,0,"V:");
+    lcd.print(10,0,veloc );
+    lcd.print(0,1,"Cal:");
+    lcd.print(4,1, calorias );
+    }
 // Fim - Eliana
+ 
+ 
+// Inicio
+// ===============================================================
+// ===                      Desafio 3                          ===
+// ===============================================================
+BlynkTimer timer;
+ 
+BLYNK_WRITE(V1)
+{
+    mulher = !mulher;
+    // Desliga o botão Homem
+    Blynk.virtualWrite(V5,LOW);
+    Blynk.setProperty(V5, "offLabel","Homem");
+}
+BLYNK_WRITE(V3)
+{
+    altura = param.asFloat();  
+}
+ 
+BLYNK_WRITE(V4)
+{
+   peso = param.asFloat(); 
+}
+ 
+BLYNK_WRITE(V2)
+{
+    homem = !homem;
+    // Desliga o botão mulher
+    Blynk.virtualWrite(V1,LOW);
+    Blynk.setProperty(V1, "offLabel","Mulher");
+}
+// Fim
+ 
 // ================================================================
 // ===                      INITIAL SETUP                       ===
 // ================================================================
-
+ 
+ 
 void setup() {
+    // Inicio - Eliana
+    Blynk.begin(auth, ssid, pass);
+    // Fim - Eliana
     // join I2C bus (I2Cdev library doesn't do this automatically)
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
         Wire.begin(D5, D6);
@@ -217,61 +286,62 @@ void setup() {
     #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
         Fastwire::setup(400, true);
     #endif
-
+ 
+ 
     // initialize serial communication
     // (115200 chosen because it is required for Teapot Demo output, but it's
     // really up to you depending on your project)
     Serial.begin(115200);
     while (!Serial); // wait for Leonardo enumeration, others continue immediately
-
+ 
     // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
     // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
     // the baud timing being too misaligned with processor ticks. You must use
     // 38400 or slower in these cases, or use some kind of external separate
     // crystal solution for the UART timer.
-
+ 
     // initialize device
     Serial.println(F("Initializing I2C devices..."));
     mpu.initialize();
     pinMode(INTERRUPT_PIN, INPUT);
-
+ 
     // verify connection
     Serial.println(F("Testing device connections..."));
     Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
+ 
     // wait for ready
     Serial.println(F("\nSend any character to begin DMP programming and demo: "));
     while (Serial.available() && Serial.read()); // empty buffer
     while (!Serial.available());                 // wait for data
     while (Serial.available() && Serial.read()); // empty buffer again
-
+ 
     // load and configure the DMP
     Serial.println(F("Initializing DMP..."));
     devStatus = mpu.dmpInitialize();
-
+ 
     // supply your own gyro offsets here, scaled for min sensitivity
     mpu.setXGyroOffset(220);
     mpu.setYGyroOffset(76);
     mpu.setZGyroOffset(-85);
     mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
-
+ 
     // make sure it worked (returns 0 if so)
     if (devStatus == 0) {
         // turn on the DMP, now that it's ready
         Serial.println(F("Enabling DMP..."));
         mpu.setDMPEnabled(true);
-
+ 
         // enable Arduino interrupt detection
         Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
         Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
         Serial.println(F(")..."));
         attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
         mpuIntStatus = mpu.getIntStatus();
-
+ 
         // set our DMP Ready flag so the main loop() function knows it's okay to use it
         Serial.println(F("DMP ready! Waiting for first interrupt..."));
         dmpReady = true;
-
+ 
         // get expected DMP packet size for later comparison
         packetSize = mpu.dmpGetFIFOPacketSize();
     } else {
@@ -283,23 +353,27 @@ void setup() {
         Serial.print(devStatus);
         Serial.println(F(")"));
     }
-
+ 
     // configure LED for output
     pinMode(LED_PIN, OUTPUT);
+    // Inicio - Eliana
     pitch1 = ypr[1] * 180/M_PI;
     pitch1 = abs(pitch1);
+    timer.setInterval(5000L, montaSaida);
+    // Fim - Eliana
 }
-
-
-
+ 
+ 
+ 
 // ================================================================
 // ===                    MAIN PROGRAM LOOP                     ===
 // ================================================================
-
+ 
 void loop() {
+ 
     // if programming failed, don't try to do anything
     if (!dmpReady) return;
-
+ 
     // wait for MPU interrupt or extra packet(s) available
     while (!mpuInterrupt && fifoCount < packetSize) {
         if (mpuInterrupt && fifoCount < packetSize) {
@@ -317,33 +391,33 @@ void loop() {
         // .
         // .
     }
-
+ 
     // reset interrupt flag and get INT_STATUS byte
     mpuInterrupt = false;
     mpuIntStatus = mpu.getIntStatus();
-
+ 
     // get current FIFO count
     fifoCount = mpu.getFIFOCount();
-
+ 
     // check for overflow (this should never happen unless our code is too inefficient)
     if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
         // reset so we can continue cleanly
         mpu.resetFIFO();
         fifoCount = mpu.getFIFOCount();
-        Serial.println(F("FIFO overflow!"));
-
+        //Serial.println(F("FIFO overflow!"));
+ 
     // otherwise, check for DMP data ready interrupt (this should happen frequently)
     } else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
         // wait for correct available data length, should be a VERY short wait
         while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
+ 
         // read a packet from FIFO
         mpu.getFIFOBytes(fifoBuffer, packetSize);
         
         // track FIFO count here in case there is > 1 packet available
         // (this lets us immediately read more without waiting for an interrupt)
         fifoCount -= packetSize;
-
+ 
         #ifdef OUTPUT_READABLE_QUATERNION
             // display quaternion values in easy matrix form: w x y z
             mpu.dmpGetQuaternion(&q, fifoBuffer);
@@ -356,7 +430,7 @@ void loop() {
             Serial.print("\t");
             Serial.println(q.z);
         #endif
-
+ 
         #ifdef OUTPUT_READABLE_EULER
             // display Euler angles in degrees
             mpu.dmpGetQuaternion(&q, fifoBuffer);
@@ -368,7 +442,7 @@ void loop() {
             Serial.print("\t");
             Serial.println(euler[2] * 180/M_PI);
         #endif
-
+ 
         #ifdef OUTPUT_READABLE_YAWPITCHROLL
             // display Euler angles in degrees
             mpu.dmpGetQuaternion(&q, fifoBuffer);
@@ -400,7 +474,7 @@ void loop() {
             Serial.print("\t");
             Serial.println(aaReal.z);
         #endif
-
+ 
         #ifdef OUTPUT_READABLE_WORLDACCEL
             // display initial world-frame acceleration, adjusted to remove gravity
             // and rotated based on known orientation from quaternion
@@ -430,10 +504,14 @@ void loop() {
             Serial.write(teapotPacket, 14);
             teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
         #endif
-
+ 
         // blink LED to indicate activity
         blinkState = !blinkState;
         digitalWrite(LED_PIN, blinkState);
+        // Inicio - Eliana
+        Blynk.run();
+        timer.run();
+        // Fim - Eliana
         //
     }
 }
